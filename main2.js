0bb0373cb0909a46a263340ca3f581de4097a491
BiancaVilapiano <bivilapiano@outlook.com>
2023-01-09 21:16:41 -0400

    1.0

---
+++ main2.js
@@ -0,0 +1,29 @@
+//importa o fs
+
+const fs = require('fs')
+// acessar a pasta, lendo os arquivos e transformando em um array
+
+const filesname = fs.readdirSync('input')
+
+// loop que faz com que o sistema leia o tamanho do array
+for(let i = 0; i < filesname.length; i++){
+
+    //variaveis que tiram as extencoes
+    let foto = filesname[i]
+
+    //replace, usado para tirar tudo a partir do caracter indicado
+    let fotosx = foto.replace(/\.[^/.]+$/, "")
+
+    //rename, renomeia a foto, conforme o indice dela, e a transforma em outra string conforme passado no ''
+    fs.rename('input/' + foto, 'output/' + fotosx + '.png', function(erro){
+        //se aparentar um erro, ira aparecer na tela uma mensagem de erro.
+        if(erro) throw erro;{
+            console.log('file renamed')
+        }
+
+
+    })
+}
+
+
+
